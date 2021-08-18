a61664f81c54e5b308e764bc472163d64849660d
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-08-18 15:08:16 +0200

    V1 complete

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # day39_Lev3_4_projekt_js-einfuehrung_einkommensteuer-berechnung
+
+**Aufgabenstellung**
+
+In diesem etwas umfangreicheren Projekt schreiben wir eine Einkommensteuer-Berechnungs-App.<br>
+Wir werden die Daten von https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2021 verwenden.<br>
+Erst definieren wir die “zu versteuerndes Einkommen” (zvE) Formel.<br>
+Das Design ist frei!
+
+Nach der Berechnung des zVE gibt es 5 Fälle die eintreten können (abhängig vom Jahreseinkommen).<br>
+Erstelle eine WebSite, mit der wir die Einkommensteuer berechnen können. Nutze dazu die Links als Referenz.<br>
+Wir sollten eine Möglichkeit haben, das Jahr zu wählen, das wir berechnen wollen.
+
+2021: <a href="https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2021">https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2021</a><br>
+2020: <a href="https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2020">https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2020</a><br>
+2019: <a href="https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2019">https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2019</a><br>
+
+---
+
+_Source:_ https://github.com/Ninni-cfm/day39_Lev3_4_projekt_js-einfuehrung_einkommensteuer-berechnung
+
+_Demo:_ https://ninni-cfm.github.io/day39_Lev3_4_projekt_js-einfuehrung_einkommensteuer-berechnung/
---
+++ assets/js/main.js
@@ -0,0 +1,268 @@
+let taxTable = [
+    year2019 = { year: 2019, max1: 9168, max2: 14254, max3: 55960, max4: 265326, max5: Infinity },
+    year2020 = { year: 2020, max1: 9408, max2: 14532, max3: 57051, max4: 270500, max5: Infinity },
+    year2021 = { year: 2021, max1: 9744, max2: 14753, max3: 57918, max4: 274612, max5: Infinity },
+];
+
+// Greeting from StackOverflow :-)
+const varToString = varObj => Object.keys(varObj)[0];
+
+const currency = 'Euro';
+
+const SingleAssessment = false;
+const SplitAssessment = !SingleAssessment;
+
+let assessmentType = SingleAssessment;
+
+let gotCmpensation = false;
+
+initialize();
+
+
+//****************************************************************************
+
+function initialize() {
+    changeAssessmentType(SingleAssessment);
+
+    formatInputNumber("txtIncome1");
+    formatInputNumber("txtIncome2");
+
+    // disabled until I know how compensation money affects tax calculation
+    // checkCompensation();
+    // formatInputNumber("txtCompensation");
+}
+
+//****************************************************************************
+
+function changeAssessmentType(newAssessmentType) {
+    assessmentType = newAssessmentType;
+
+    document.getElementById("lblIncome2").style.display =
+        document.getElementById("blkIncome2").style.display =
+        assessmentType == SingleAssessment ? "none" : "inherit";
+}
+
+// disabled until I know how this money affects tax calculation
+// function checkCompensation() {
+//     gotCompensation = document.getElementById("chkCompensation").checked;
+//     document.getElementById("noCompensation").style.display =
+//         gotCompensation ? "none" : "inline-block";
+
+//     document.getElementById("gotCompensation").style.visibility =
+//         !gotCompensation ? "hidden" : "visible";
+// }
+
+//****************************************************************************
+
+function startTaxCalculation() {
+
+    let year = Number(document.getElementById("yearSelection").value);
+    let income = Number(document.getElementById("txtIncome1").value);
+
+    // if income of partner is valuable as well
+    if (assessmentType == SplitAssessment) {
+        income += Number(document.getElementById("txtIncome2").value);
+        income /= 2;
+    }
+
+    // only the integer part of the value is used for tax calculation
+    income = Math.floor(income);
+
+    let incomeTax = taxCalculation(year, income);
+    if (assessmentType == SplitAssessment) {
+        incomeTax *= 2;
+    }
+
+    let churchTaxRate = document.getElementById("churchTaxSelection").value;
+    let churchTax = incomeTax * churchTaxRate / 100;
+
+    setNumberInnerHTML("incomeTax", incomeTax);
+    setNumberInnerHTML("churchTax", churchTax);
+    setNumberInnerHTML("totalTaxes", incomeTax + churchTax);
+
+}
+
+//----------------------------------------------------------------------------
+function taxCalculation(year, income) {
+    let taxes = 0;
+    switch (year) {
+        case 2021:
+            taxes = formula2021(income, year2021);
+            break;
+        case 2020:
+            taxes = formula2020(income, year2020);
+            break;
+        case 2019:
+            taxes = formula2019(income, year2019);
+            break;
+    }
+
+    return taxes;
+}
+
+//----------------------------------------------------------------------------
+function formula2021(income, limits) {
+
+    let chargeableIncome = income;
+    // did we receive some compensation?
+    if (gotCompensation) {
+        chargeableIncome += Number(document.getElementById("txtCompensation").value) * 2;
+    }
+
+    let taxes = 0;
+    switch (true) {
+        case chargeableIncome <= limits.max1:
+            break;
+        case chargeableIncome <= limits.max2:
+            taxes = tax2021Formular2(income);
+            break;
+        case chargeableIncome <= limits.max3:
+            taxes = tax2021Formular3(income);
+            break;
+        case chargeableIncome <= limits.max4:
+            taxes = tax2021Formular4(income);
+            break;
+        default:
+            taxes = tax2021Formular5(income);
+            break;
+    }
+    return Math.floor(taxes);
+}
+function formula2020(income, limits) {
+
+    let taxes = 0;
+    switch (true) {
+        case income <= limits.max1:
+            break;
+        case income <= limits.max2:
+            taxes = tax2020Formular2(income);
+            break;
+        case income <= limits.max3:
+            taxes = tax2020Formular3(income);
+            break;
+        case income <= limits.max4:
+            taxes = tax2020Formular4(income);
+            break;
+        default:
+            taxes = tax2020Formular5(income);
+            break;
+    }
+    return Math.floor(taxes);
+}
+function formula2019(income, limits) {
+
+    let taxes = 0;
+    switch (true) {
+        case income <= limits.max1:
+            break;
+        case income <= limits.max2:
+            taxes = tax2019Formular2(income);
+            break;
+        case income <= limits.max3:
+            taxes = tax2019Formular3(income);
+            break;
+        case income <= limits.max4:
+            taxes = tax2019Formular4(income);
+            break;
+        default:
+            taxes = tax2019Formular5(income);
+            break;
+    }
+    return Math.floor(taxes);
+}
+
+//----------------------------------------------------------------------------
+// common for all years: no money, no taxes :-)
+function taxAllFormular1() {
+    return 0;
+}
+
+//----------------------------------------------------------------------------
+function tax2021Formular2(income) {
+    let y = (income - 9744) / 10000;
+    let taxes = (995.21 * y + 1400) * y;
+    return taxes;
+}
+function tax2021Formular3(income) {
+    let z = (income - 14753) / 10000;
+    let taxes = (208.85 * z + 2397) * z + 950.96;
+    return taxes;
+}
+function tax2021Formular4(income) {
+    return 0.42 * income - 9136.63;
+}
+function tax2021Formular5(income) {
+    return 0.45 * income - 17374.99;
+}
+
+//----------------------------------------------------------------------------
+function tax2020Formular2(income) {
+    let y = (income - 9408) / 10000;
+    let taxes = (972.87 * y + 1400) * y;
+    return taxes;
+}
+function tax2020Formular3(income) {
+    let z = (income - 14532) / 10000;
+    let taxes = (212.02 * z + 2397) * z + 972.79;
+    return taxes;
+}
+function tax2020Formular4(income) {
+    return 0.42 * income - 8963.74;
+}
+function tax2020Formular5(income) {
+    return 0.45 * income - 17078.74;
+}
+
+//----------------------------------------------------------------------------
+function tax2019Formular2(income) {
+    let y = (income - 9168) / 10000;
+    let taxes = (972.87 * y + 1400) * y;
+    return taxes;
+}
+function tax2019Formular3(income) {
+    let z = (income - 14254) / 10000;
+    let taxes = (216.16 * z + 2397) * z + 965.58;
+    return taxes;
+}
+function tax2019Formular4(income) {
+    return 0.42 * income - 8780.90;
+}
+function tax2019Formular5(income) {
+    return 0.45 * income - 16740.68;
+}
+
+
+//****************************************************************************
+// helper functions
+
+function setNumberInnerHTML(id, value) {
+
+    document.getElementById(id).innerHTML = formatNumber(value, true);
+}
+
+function formatInputNumber(id) {
+
+    let income = Number(document.getElementById(id).value);
+    document.getElementById(id).value = formatNumber(income, false);
+}
+
+
+
+//----------------------------------------------------------------------------
+// format a number 
+function formatNumber(value, showCurrencySymbol = true) {
+
+    let formattedNumber = (Math.floor(value * 100) / 100).toFixed(2);
+
+    if (showCurrencySymbol) {
+        formattedNumber += ` ${currency}`;
+    }
+    return formattedNumber;
+}
+
+
+//----------------------------------------------------------------------------
+// Debugging helpers
+function debugVar(expr) {
+    console.log(varToString(expr) + ": ", eval(expr));
+}
